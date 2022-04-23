8b482781cf3a5da26f4faef3cc88bee844f602ff
acebuill <alidy@mailfence.com>
2022-04-23 20:22:36 -0300

    compact code

--- src/pacmanconfig.py
+++ src/pacmanconfig.py
@@ -1,24 +1,15 @@
-import os
-import sys
-import typing
+import os, sys, typing, re
 import fileinput
-import re
 from log import log, log_err, log_ok
 
 
-rgx = typing.NewType("rgx", re.Pattern)
+get_env = lambda varname: os.getenv(varname.upper())
 
 
-def get_env_var(variable_name: str) -> str:
-    return os.getenv(variable_name.upper())
-
-
-def has_write_permissions(file: str) -> bool:
-    return os.access(file, os.W_OK)
-
-
-def get_artix_arch_mirror_path(mirror_name: str) -> str:
-    available_mirrors = [
+repo_mirror_path = (
+    lambda repo_name: "/etc/pacman.d/mirrorlist-arch"
+    if repo_name
+    in [
         "multilib",
         "extra",
         "community",
@@ -26,89 +17,87 @@ def get_artix_arch_mirror_path(mirror_name: str) -> str:
         "community-testing",
         "multilib-testing",
     ]
-    if mirror_name in available_mirrors:
-        return "/etc/pacman.d/mirrorlist-arch"
-    raise Exception(f"{mirror_name} is not an artix arch supported mirror")
+    else Exception(repo_name + " is not implemented yet ...")
+)
 
 
-def get_mirror_include_path(mirror_name: str) -> str:
-    artix_arch_mirror_path = "/etc/pacman.d/mirrorlist-arch"
-    if os.path.exists(artix_arch_mirror_path):
-        return get_artix_arch_mirror_path(mirror_name)
+repo_include_string = (
+    lambda repo_name, include_path: f"\n[{repo_name}]\nInclude = {include_path}\n"
+)
+
+
+has_write_permissions = lambda file_name: os.access(file_name, os.W_OK)
+
+
+pacman_conf_path = (
+    lambda testing: "/etc/pacman.conf" if not testing else "pacman.conf"
+)
+
+
+append_to_configuration_file = lambda string: open(
+    pacman_conf_path(False), "a"
+).write(string)
+
+
+repo_is_enabled = lambda repo_name, pacman_config_file: re.compile(
+    f"[^#]\\[{repo_name}\\]"
+).search(open(pacman_config_file, "r").read())
+
+
+repository_rgx = lambda repo_name: re.compile(
+    f"(?:\n)?\\[{repo_name}\\]\nInclude(?: )?=(?: )?/.*(?:\n)?"
+)
+
+
+set_inline_parallel_downloads = (
+    lambda parallel_n: lambda line: print(f"ParallelDownloads = {parallel_n}")
+    if "ParallelDownloads" in line
+    else print(line, end="")
+)
+
+
+def enable_repository(repo_name):
+    if repo_is_enabled(repo_name, pacman_conf_path(False)):
+        log_ok(f"{repo_name} is already enabled")
     else:
-        raise Exception(f"{mirror_name} include path not implemented")
-
-
-def mirror_is_enabled(
-    mirror_name: str, pacman_config_path: str = "/etc/pacman.conf"
-) -> bool:
-    with open(pacman_config_path, "r") as file_:
-        return bool(
-            re.compile(f"[^#]\\[{mirror_name}\\]").search(file_.read())
-        )
-
-
-def enable_repository(
-    mirror_name: str, pacman_config_path: str = "/etc/pacman.conf"
-) -> bool:
-    is_root = get_env_var("user") == "root"
-    if mirror_is_enabled(mirror_name, pacman_config_path):
-        log_ok(f"{mirror_name} is already enabled")
-        return True
-    if has_write_permissions(pacman_config_path):
-        with open(pacman_config_path, "a") as pconfig:
-            log(f"enabling {mirror_name}")
-            pconfig.write(
-                f"\n[{mirror_name}]\nInclude = {get_mirror_include_path(mirror_name)}\n"
+        if has_write_permissions(pacman_conf_path(False)):
+            append_to_configuration_file(
+                repo_include_string(
+                    repo_name, repo_mirror_path(repo_name)
+                )
             )
-        log_ok(f"enabled {mirror_name}")
-        return True
-    else:
-        raise Exception(
-            f"not enoght permissions to write to {pacman_config_path}"
-        )
-        return False
+        else:
+            raise Exception("config file does not have write permissions")
+        log_ok(f"enabled {repo_name}")
 
 
-def delete_repository_line(
-    repository_name: str, pacman_config_path: str = "/etc/pacman.conf"
-) -> bool:
+def delete_repository_line(repo_name, pacman_config_path="/etc/pacman.conf"):
     if has_write_permissions(pacman_config_path):
-        repository_regex = re.compile(
-            f"(?:\n)?\\[{repository_name}\\]\nInclude(?: )?=(?: )?/.*(?:\n)?",
-            re.IGNORECASE,
-        )
         with open(pacman_config_path, "r+") as file_:
-            new_file = re.sub(repository_regex, "", file_.read())
+            new_file = re.sub(repository_rgx(repo_name), "", file_.read())
             file_.seek(0)
             file_.write(new_file)
             file_.truncate()
-            return True
     else:
         raise Exception(
             f"root access is required in order to write to {pacman_config_path}"
         )
-        return False
 
 
-def disable_repository(
-    repository_name: str, pacman_config_path: str = "/etc/pacman.conf"
-) -> bool:
-    log(f"disabling {repository_name}")
-    delete_repository_line(repository_name, pacman_config_path)
-    log_ok(f"disabled {repository_name}")
+def disable_repository(repo_name: str, pacman_config_path="/etc/pacman.conf"):
+    log(f"disabling {repo_name}")
+    delete_repository_line(repo_name, pacman_config_path)
+    log_ok(f"disabled {repo_name}")
     return True
 
 
-def set_number_of_parallel_downloads(n: int, pacman_config_path: str) -> bool:
-    is_root = get_env_var("user") == "root"
-    if not is_root:
-        return False
-    for line in fileinput.input(pacman_config_path, inplace=1):
-        if "ParallelDownloads" in line:
-            line = f"ParallelDownloads = {n}"
-        sys.stdout.write(line)
-    return True
+def set_number_of_parallel_downloads(n: int):
+    if has_write_permissions(pacman_conf_path(False)):
+        for line in fileinput.input(pacman_conf_path(False), inplace=1):
+            set_inline_parallel_downloads(n)(line)
+        log_ok(f"set to {n} parallel downloads at the same time")
+    else:
+        log_err(f"either not enough permissions or config doesn't exist")
 
 
 def check_include_path(path: str) -> bool:
